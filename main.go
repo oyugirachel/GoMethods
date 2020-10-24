b33ff0214a2efaadf5f31fcacac24a13d5f5a6c8
oyugirachel <oyugirachel20@gmail.com>
2020-10-24 14:04:26 +0300

    go methods

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	u := User{"Rachel", "Oyugi"}
 	fmt.Println(u.Greeting())
+	p:=Occupation{"Software Developer", "Information Technology"}
+	fmt.Println(p.Job())
 
 	
 }
@@ -12,6 +14,12 @@ type User struct {
 	FirstName, LastName string
 }
 
+type Occupation struct{
+	Title, Department string
+}
 func (u User) Greeting() string {
 	return fmt.Sprintf("Dear %s %s", u.FirstName, u.LastName)
+}
+func (p Occupation) Job() string{
+	return fmt.Sprintf("Rachel is a %s  in the %s  Department", p.Title, p.Department)
 }
